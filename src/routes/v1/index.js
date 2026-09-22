982a49dc108a96a2e36d48485d56fe1691835272
DUQUE2414 <danielduagu1@gmail.com>
2026-09-22 13:02:03 -0500

    feat: Cambio de rutas URL de español a ingles

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersRoutes = require("./users.routes");
 
 // Monta el recurso en /api/v1/usuarios
-router.use("/usuarios", usersRoutes);
+
+router.use('/users', usersRoutes);
 
 module.exports = router;
